d5171490f3d95f3c5729071b64718bae40f92ba1
Karthik Jagadeesh <karthikjagadeesh@gmail.com>
2011-03-02 01:56:31 -0800

    added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
   end
 
   describe "GET 'contact'" do
@@ -16,4 +18,10 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
