50c51e612190b21fedaa87b11d7ce47b94f45380
Michael Chang <aznm1ke31@gmail.com>
2011-03-05 21:44:51 -0800

    added "show" for users, finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Recipes::Application.routes.draw do
   # get "pages/contact"
 
   # get "pages/about"
- 
+
+  # generates REST accessors 
+  resources :users
+
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
