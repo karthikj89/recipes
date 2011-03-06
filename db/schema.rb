919d35a977d26d378a25d665dd518ab51f5bbc6f
Michael Chang <aznm1ke31@gmail.com>
2011-03-05 21:27:59 -0800

    added validations for users added db index validation for email attribute of users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110304085841) do
+ActiveRecord::Schema.define(:version => 20110306052512) do
 
   create_table "dishes", :force => true do |t|
     t.string   "name"
@@ -60,5 +60,7 @@ ActiveRecord::Schema.define(:version => 20110304085841) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,73 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # runs the enclosed code before each test
+  before(:each) do
+    @attr = { :first_name => "Example", :last_name => "User", :email => "user@example.com" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    # create! raises an exception if something goes wrong
+    User.create!(@attr)
+  end
+
+  it "should require a last_name" do
+    no_last_name_user = User.new(@attr.merge(:first_name => ""))
+    no_last_name_user.should_not be_valid
+  end
+
+  it "should require a last_name" do
+    no_last_name_user = User.new(@attr.merge(:last_name => ""))
+    no_last_name_user.should_not be_valid
+  end
+
+  it "should require an email" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
+  it "should reject last_names that are too long" do
+    long_last_name_user = User.new(@attr.merge(:first_name => "a" * 51))
+    long_last_name_user.should_not be_valid
+  end
+
+  it "should reject last_names that are too long" do
+    long_last_name_user = User.new(@attr.merge(:last_name => "a" * 51))
+    long_last_name_user.should_not be_valid
+  end
+  
+  # TODO more robust test cases
+  it "should accept valid email addresses" do
+    # creates a string array from its elements...
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@food.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate emails" do
+    User.create!(@attr)
+    duplicate_user = User.new(@attr)
+    duplicate_user.should_not be_valid
+  end
+
+  it "should reject duplicate emails regardless of case" do
+    User.create!(@attr)
+    # upcased_email = @attr[:email].upcase
+    duplicate_user = User.new(@attr.merge(:email => @attr[:email].upcase))
+    duplicate_user.should_not be_valid
+  end
+
+
+
 end
