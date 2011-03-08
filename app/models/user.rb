9d8a00b7e279b42940f713e336714816757e7df8
Stephen Chu <chu.stephen@gmail.com>
2011-03-07 22:19:38 -0800

    Adding a dummy encryption for User passwords. Michael, hopefully this doesn't trample on any work you've done recently. This is mainly for more practice with git :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,29 @@ class User < ActiveRecord::Base
   validates :password,    :presence     => true,
                           :confirmation => true,
                           :length       => { :within => 8..40 }
+
+  # we register a callback called encrypt_password by passing a 
+  # symbol of that name to the before_save method, and then define
+  # an encrypt_password method to perform the encryption.
+  #
+  # By using before_save, Active Record automatically calls the
+  # corresponding method (in this case, encrypt_password) before
+  # saving the record.
+  before_save :encrypt_password
+
+
+  # private methods
+  private  
+    # this extra indention is a convention. though unnecessary,
+    # it helps reinforce the fact that the methods declared after
+    # the "private" keyterm are used internally by the object.
+
+    def encrypt_password
+      # the argument on the right hand side is implicitly self.password
+      self.encrypted_password = encrypt( password )
+    end
+
+    def encrypt( string )
+      string
+    end
 end
